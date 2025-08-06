b2246aa6382aea887eb0aa8f927823666f49d88c
Anastasia <kornewaanas@gmail.com>
2025-08-06 19:28:46 +0300

    fix port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { title } from 'process'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const HTTP_STATUSES = {
   OK_200: 200,
